d92e7210086e9534f09de8659eacc50c4bfe6792
Ethan Wood <woodethx@gmail.com>
2025-03-22 21:49:54 -0500

    Add draw and erase buttons

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
 const container = document.querySelector("#container");
+const erase = document.querySelector("#erase");
+const draw = document.querySelector("#draw");
 for (let i = 0; i < 256; i++) {
     container.appendChild(document.createElement("div"));
 }
 
+
 const squares = document.querySelectorAll("#container > div");
 console.log(squares);
+
+
+draw.addEventListener("click", () =>{
+squares.forEach(div => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    })
+})
+});
+
+erase.addEventListener("click", () =>{
+    squares.forEach(div => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "white";
+        })
+    })
+});
+
